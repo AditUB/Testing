e96b385bd681c06741bb166936a96518e8107ec4
AUB <adit.ignite@gmail.com>
2022-10-16 22:18:50 +0530

    Diamond pattern A

--- JAVA_code/src/pattern.java
+++ JAVA_code/src/pattern.java
@@ -4,17 +4,49 @@ class pattern {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i + 1; j++)
-                System.out.print("*");
-            for (int k = 1; k <= 2 * i + 1; k++)
-                System.out.print(" ");
-            for (int j = 1; j < n - i + 1; j++)
-                System.out.print("*");
+        int n = sc.nextInt();
+        diamond(n);
+    }
+
+    static void diamond(int n) {
+
+        // hollow diamond
+        for (int i = -n; i <= n; i++) {
+            for (int j = -n; j <= n; j++) {
+                if ((Math.abs(i) + Math.abs(j)) >= n)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+
+            }
             System.out.println();
         }
+
+        // diamond border
+        for (int i = -n; i <= n; i++) {
+            for (int j = -n; j <= n; j++) {
+                if ((Math.abs(i) + Math.abs(j)) == n)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+
+            }
+            System.out.println();
+        }
+
+        // filled diamond
+        for (int i = -n; i <= n; i++) {
+            for (int j = -n; j <= n; j++) {
+                if ((Math.abs(i) + Math.abs(j)) <= n)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+
+            }
+            System.out.println();
+        }
+
     }
 
 }
